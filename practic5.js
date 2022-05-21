a8cf6bf3c508abb92908672e73ada774eac92a25
kategrom <95121842+kategrom@users.noreply.github.com>
2022-05-21 14:57:22 +0300

    Add files via upload

---
+++ practic5.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Task 1
+
+
+// for (let i = 0; i < 11; i++) {
+//   console.log(i + 1);
+// }
+
+
+// Task 2
+
+
+// for (let i = 0; i < 11; i++) {
+//   if ((i % 2) === 0) {
+//     console.log('Число парне:', i);
+//   }else(
+//     console.log('Число непарне:', i)
+//   )
+// }
+
+
+// Task 3
+
+
+// let minNum = 20;
+// const maxNum = 28;
+
+// while (minNum < maxNum) {
+//   console.log(minNum);
+//   minNum += 1;
+// }
+
+
+// Task 4
+
+// let hero = prompt("ВВедіть ім'я героя");
+// while (hero.lenght < 6){
+//   console.log(hero);
+//   hero = prompt("Введіть ім'я героя");
+// }
+
+//   console.log("Кількість букв менше 6");
